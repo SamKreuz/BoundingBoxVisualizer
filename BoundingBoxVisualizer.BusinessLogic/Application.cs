d846779994224e0d8f12c5fec5c5a356080d001e
Samuel Kreuz <sam@samuelkreuz.com>
2023-05-11 18:33:21 +0200

    Added Revit 2023 references, created PickElement method

---
+++ BoundingBoxVisualizer.BusinessLogic/Application.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.UI;
+
+namespace BoundingBoxVisualizer.BusinessLogic
+{
+    public class Application : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BoundingBoxVisualizer.BusinessLogic/Run.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+
+namespace BoundingBoxVisualizer.BusinessLogic
+{
+    internal class Run : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDocument = commandData.Application.ActiveUIDocument;
+
+            Element element = PickElement(uiDocument);
+
+            if(element == null)
+            {
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        private Element PickElement(UIDocument uiDocument)
+        {
+            Reference elementReference = null;
+
+            try
+            {
+                elementReference = uiDocument.Selection.PickObject(ObjectType.Element);
+            }
+            catch(Exception e)
+            {
+                // TODO SK
+                return null;
+            }
+
+            if(elementReference == null)
+            {
+                // TODO SK;
+            }
+
+            Element element = uiDocument.Document.GetElement(elementReference);
+
+            return element;
+        }
+    }
+}
