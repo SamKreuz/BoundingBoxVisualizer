cf9fcf7c4a5d0a0ab2e699fd002cdb20df6b0045
Samuel Kreuz <sam@samuelkreuz.com>
2023-05-12 17:39:27 +0200

    Added method to create solid from bounding box

--- BoundingBoxVisualizer.BusinessLogic/Logic/Geometry/GeometryCreator.cs
+++ BoundingBoxVisualizer.BusinessLogic/Logic/Geometry/GeometryCreator.cs
@@ -42,5 +42,37 @@ namespace BoundingBoxVisualizer.BusinessLogic.Logic
 
             return transformedSolis;
         }
+
+        public Solid CreateGeometryFromBoundingBox(BoundingBoxXYZ boundingBox)
+        {
+            XYZ min = boundingBox.Min;
+            XYZ max = boundingBox.Max;
+            //var transform = boundingBox.Transform;
+
+            // Offset to prevent z-fighting
+            double offset = 0.02;
+            XYZ offsetVal = new XYZ(offset, offset, offset);
+            min -= offsetVal;
+            max += offsetVal;
+
+            XYZ pointA = new XYZ(min.X, min.Y, min.Z);
+            XYZ pointB = new XYZ(min.X, max.Y, min.Z);
+            XYZ pointC = new XYZ(max.X, max.Y, min.Z);
+            XYZ pointD = new XYZ(max.X, min.Y, min.Z);
+
+            CurveLoop profile = new CurveLoop();
+            profile.Append(Line.CreateBound(pointA, pointB));
+            profile.Append(Line.CreateBound(pointB, pointC));
+            profile.Append(Line.CreateBound(pointC, pointD));
+            profile.Append(Line.CreateBound(pointD, pointA));
+            List<CurveLoop> profiles = new List<CurveLoop> { profile };
+
+            XYZ extrusionDirection = XYZ.BasisZ;
+            double extrusionDistance = max.Z - min.Z; 
+
+            var solid = GeometryCreationUtilities.CreateExtrusionGeometry(profiles, extrusionDirection, extrusionDistance);
+
+            return solid;
+        }
     }
 }
--- BoundingBoxVisualizer.BusinessLogic/Logic/ServiceUtility.cs
+++ BoundingBoxVisualizer.BusinessLogic/Logic/ServiceUtility.cs
@@ -11,23 +11,28 @@ namespace BoundingBoxVisualizer.BusinessLogic.Logic
         {
             ExternalServiceId serviceId = ExternalServices.BuiltInExternalServices.DirectContext3DService;
             ExternalService service = ExternalServiceRegistry.GetService(serviceId);
+            var geometryCreater = new GeometryCreator();
+            var bb = geometryElement.GetBoundingBox();
+            var t = bb.Transform;
 
-            Solid solid = new GeometryCreator().CreateCenterbasedBox(XYZ.Zero, 10);
+            Solid solid = geometryCreater.CreateGeometryFromBoundingBox(bb);
+            //Solid solid = geometryCreater.CreateCenterbasedBox(XYZ.Zero, 10);
             
             //Transparency is value between 0-255
-            var color = new ColorWithTransparency(255, 0, 0, 200);
+            var colorRed = new ColorWithTransparency(255, 0, 0, 200);
+            var colorGreen = new ColorWithTransparency(0, 255, 0, 200);
 
-            var painterElement = new Painter(geometryElement, color);
-            var painterGeometry = new Painter(solid, color);
+            //var painterElement = new Painter(geometryElement, colorRed);
+            var painterGeometry = new Painter(solid, colorGreen);
 
-            service.AddServer(painterElement);
+            //service.AddServer(painterElement);
             service.AddServer(painterGeometry);
 
             // TODO SK: Check if needed. Probably only for multiple servers;
             MultiServerService msDirectContext3DService = service as MultiServerService;
             
             IList<Guid> serverIds = msDirectContext3DService.GetActiveServerIds();
-            serverIds.Add(painterElement.GetServerId());
+            //serverIds.Add(painterElement.GetServerId());
             serverIds.Add(painterGeometry.GetServerId());
 
             msDirectContext3DService.SetActiveServers(serverIds);
